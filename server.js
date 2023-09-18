fe80068cbc8ce1646460d3302a4bd4bc1308350e
akilinjamam <akilinjamam@gmail.comm>
2023-09-18 14:00:20 +0600

    file-updated

--- server.js
+++ server.js
@@ -5,14 +5,20 @@ const colors = require('colors');
 const app = require("./app");
 
 // Database:
-const database = module.exports = () => {
-    const connectionParams = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    }
+const database = module.exports = async () => {
+    // const connectionParams = {
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    // }
 
     try {
-        mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2gfgxwp.mongodb.net/?retryWrites=true&w=majority`, connectionParams);
+        await mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2gfgxwp.mongodb.net/?retryWrites=true&w=majority`);
+
+        const port = process.env.PORT
+
+        app.listen(port, () => {
+            console.log(`app is running on port : ${port}`.blue.bold);
+        })
         console.log('Database connected successfully yeah...')
     } catch (error) {
         console.log('database connection falied');
@@ -24,8 +30,4 @@ const database = module.exports = () => {
 database();
 
 
-const port = process.env.PORT || 8080;
 
-app.listen(port, () => {
-    console.log(`app is running on port : ${port}`.blue.bold);
-})
