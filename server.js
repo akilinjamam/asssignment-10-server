42f3d0d3b95f72d458d3a5ba791a272040a2c277
akilinjamam <akilinjamam@gmail.comm>
2023-09-19 20:21:20 +0600

    file-updated

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const colors = require('colors');
 const app = require("./app");
 
 // Database:
-const database = module.exports = () => {
+const database = () => {
     const connectionParams = {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     }
 
-    mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.2gfgxwp.mongodb.net/?retryWrites=true&w=majority`, connectionParams);
+    mongoose.connect(`${process.env.DB_CONNECTION_URI}`, connectionParams);
     console.log('Database connected successfully yeah...')
 
 }
