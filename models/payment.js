b95003322825c6dbb8dcb99d0157f05f6e2839df
akilinjamam <akilinjamam@gmail.comm>
2023-10-07 23:48:55 +0600

    file-updated

--- models/payment.js
+++ models/payment.js
@@ -142,6 +142,10 @@ const paymentSchema = mongoose.Schema(
             type: String,
             required: true
         },
+        tourId: {
+            type: String,
+            required: true
+        },
         nameData: [
             {
                 member1: {
