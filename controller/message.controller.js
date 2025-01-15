d77d9c71c603e339e6fbfc21c38bb3968d97301d
akilinjamam <akilinjamam@gmail.com>
2025-01-16 05:30:54 +0600

    Removed sensitive information

--- controller/message.controller.js
+++ controller/message.controller.js
@@ -1,26 +1,26 @@
-const { saveMessageService } = require("../services/message.service")
-const https = require('follow-redirects').https;
-const accountSid = process.env.ACCOUNT_SID;
-const authToken = process.env.AUTH_TOKEN;
-// const client = require('twilio')(accountSid, authToken);
-const twilio = require('twilio')
+// const { saveMessageService } = require("../services/message.service")
+// const https = require('follow-redirects').https;
+// const accountSid = process.env.ACCOUNT_SID;
+// const authToken = process.env.AUTH_TOKEN;
+// // const client = require('twilio')(accountSid, authToken);
+// const twilio = require('twilio')
 
 
 
-module.exports.createMessageController = async (req, res) => {
+// module.exports.createMessageController = async (req, res) => {
 
-    try {
-        const client = new twilio(accountSid, authToken)
+//     try {
+//         const client = new twilio(accountSid, authToken)
 
-        client.messages.create({
-            body: 'hello',
-            to: req.body.message,
-            from: '+12092645211'
-        })
-    } catch (error) {
-        res.status(400).json({
-            message: error.message
-        })
-    }
+//         client.messages.create({
+//             body: 'hello',
+//             to: req.body.message,
+//             from: '+12092645211'
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             message: error.message
+//         })
+//     }
 
-}
+// }
